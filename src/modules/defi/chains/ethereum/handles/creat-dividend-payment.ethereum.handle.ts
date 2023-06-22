05abcfb6c163597209add1f9d89bac544da5c60d
helilabs-tn2 <tn2.hl@heli-cap.com>
2023-06-22 15:53:42 +0700

    update contract fund make payment

--- src/modules/defi/chains/ethereum/handles/creat-dividend-payment.ethereum.handle.ts
+++ src/modules/defi/chains/ethereum/handles/creat-dividend-payment.ethereum.handle.ts
@@ -1,19 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { TransferSingleBaseHandle } from 'src/modules/defi/handles/transfer-single.base.handle';
-import creatDividendPayment from './abis/creat-dividend-payment.abi.json';
+import creatDividendPaymentABI from './abis/creat-dividend-payment.abi.json';
 import { CancelLoanService } from 'src/modules/defi/gateway/cancel-loan/cancel-loan.service';
 @Injectable()
 export class CreatDividendPaymentEthereumHandle extends TransferSingleBaseHandle {
   constructor(configService: ConfigService, service: CancelLoanService) {
     super('creatDividendPayment', configService, service);
-    this.abi = creatDividendPayment;
+    this.abi = creatDividendPaymentABI;
     this.mappings = [
-      { key: 'issuers', convert: undefined },
+      { key: 'issuer', convert: undefined },
       { key: 'totalAmount', convert: this.toNumber },
-      { key: 'historyLen', convert: undefined },
+      { key: 'historyLen', convert: this.toNumber },
       { key: 'paymentType', convert: this.toNumber },
       { key: 'root', convert: undefined },
+      { key: 'uuid', convert: this.toNumber },
     ];
     this.logNames = ['creatDividendPayment'];
   }
